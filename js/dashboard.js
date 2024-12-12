bb7ea29b5e12079722c4c17a5723f18794c085a6
zej8512 <zej85.12@gmail.com>
2024-12-12 16:00:23 +0800

    提交訊息12/12

---
+++ js/dashboard.js
@@ -0,0 +1,19 @@
+// header
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
+
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     var toggler = document.querySelector('.navbar-toggler');
+    //     var navbar = document.querySelector('#navbarNav');
+        
+    //     toggler.addEventListener('click', function() {
+    //         navbar.classList.toggle('show');
+    //     });
+    // });
+
